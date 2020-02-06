634a24f4da6190de1f0a0857082eec039ca27047
Ionut Simion <ionut@simion.co>
2020-02-06 02:24:34 +0200

    * Updated [top level] CMakeLists.txt + Added a config input file * Print the version in the test file "executable"

--- tests/wrapper_tests.cxx
+++ tests/wrapper_tests.cxx
@@ -1,6 +1,16 @@
 #include <wrapper.hxx>
+#include <wrappers_config.hxx>
 
-int main()
+#include <iostream>
+
+int main(int const number_of_arguments, char *arguments[])
 {
+    std::cout
+        << arguments[0]
+        << " Version " << is::wrappers::version::major
+        << '.' << is::wrappers::version::minor
+        << '.' << is::wrappers::version::patch
+        << std::endl;
+
     return 0;
 }
